4a7cfee378a5797da8849ee6ab185564e63130e3
Oleg <olegmama@gmail.com>
2023-09-01 15:49:15 +0300

    modals

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,41 +1,50 @@
-import { Component, ElementRef, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from 'src/app/shared/modals/login/login.component';
 import { DeliveryModalComponent } from '../../shared/modals/delivery/delivery-modal/delivery-modal.component';
-
-
+import { DataService } from '../../shared/services/dataService/data.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  constructor(private elRef: ElementRef,private dialog: MatDialog) {}
+export class HeaderComponent implements OnInit {
+  constructor(
+    private elRef: ElementRef,
+    private dialog: MatDialog,
+    private dataService: DataService
+  ) {}
 
   @HostListener('document:click', ['$event'])
-    onClick(event: MouseEvent) {
-        const menuBlock = this.elRef.nativeElement.querySelector('.menu-block');
-        const burger = this.elRef.nativeElement.querySelector('#burger');
+  onClick(event: MouseEvent) {
+    const menuBlock = this.elRef.nativeElement.querySelector('.menu-block');
+    const burger = this.elRef.nativeElement.querySelector('#burger');
 
-        if (!menuBlock.contains(event.target) && !burger.contains(event.target)) {
-            this.closeBlock();
-        }
+    if (!menuBlock.contains(event.target) && !burger.contains(event.target)) {
+      this.closeBlock();
     }
-
+  }
 
   public show = false;
   public layer = false;
 
-    // burger menu
-    burger() {
-      this.show = !this.show;
-      this.layer = !this.layer;
-      const body = document.getElementsByTagName('body')[0];
-      body.classList.toggle('lockBurger');
-    }
+  public message = 'натисніть для заміни';
+  // burger menu
+  burger() {
+    this.show = !this.show;
+    this.layer = !this.layer;
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.toggle('lockBurger');
+  }
 
-    
+  ngOnInit(): void {
+    this.dataService.changeMessage(this.message);
+    this.dataService.currentMessage.subscribe((message) => {
+      this.message = message;
+    });
+  }
+
   //  close
   closeBlock() {
     this.show = false;
@@ -44,16 +53,13 @@ export class HeaderComponent {
     body.classList.remove('lockBurger');
   }
 
-   //  open modal login
-   openModalLogin() {
+  //  open modal login
+  openModalLogin() {
     this.dialog.open(LoginComponent);
   }
 
-     //  open modal delivery
-     openModalDelivery() {
-      this.dialog.open(DeliveryModalComponent);
-    }
-
-
-
+  //  open modal delivery
+  openModalDelivery() {
+    this.dialog.open(DeliveryModalComponent);
+  }
 }
--- src/app/shared/modals/delivery/delivery-modal/delivery-modal.component.ts
+++ src/app/shared/modals/delivery/delivery-modal/delivery-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../../services/dataService/data.service';
+
 
 @Component({
   selector: 'app-delivery-modal',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./delivery-modal.component.scss']
 })
 export class DeliveryModalComponent {
+  constructor(private dataService: DataService) { }
+  message: string = 'Доставка кур’єром'; // Початковий текст
+
+  onClickLink(newText: string) {
+    // При кліку на лінк змінюємо innerHTML в іншому компоненті
+    this.message = newText; // Змінюємо обраний текст при кліку
+
+    this.dataService.changeMessage(this.message);  }
 
 }
